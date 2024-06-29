3f154c359895df028ef74a3b0ffc274fa4b43d88
Raj Kashikar <65059626+raj200501@users.noreply.github.com>
2024-06-29 01:16:07 -0700

    Create RecommendationsFragment.kt

---
+++ app/src/main/UI/RecommendationsFragment.kt
@@ -0,0 +1,17 @@
+package com.example.healthmonitor.UI
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.healthmonitor.R
+
+class RecommendationsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_recommendations, container, false)
+    }
+}
